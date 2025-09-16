800ce5dd31397ec56fbeb097b0797962a4237560
tomasgroque <tomas.groque@gmail.com>
2025-09-15 18:13:51 -0600

    updating script.py

--- script.py
+++ script.py
@@ -7,21 +7,36 @@ load_dotenv()
 
 POLYGON_API_KEY = os.getenv('POLYGON_API_KEY')
 LIMIT = 1000
-url = f'"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}'
+url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}"
 
 response = requests.get(url)
 
 tickers = []
-data = response.json()
-for ticker in data['results']:
-    tickers.append(ticker)
+raw_data = response.json()
+data = raw_data['results']
 
+for ticker in data:
+    tickers.append(ticker)
 
 while 'next_url' in data:
     response = requests.get(data['next_url'] + f'?&apiKey={POLYGON_API_KEY}')
-    data = response.json()
+    raw_data = response.json()
+    data = raw_data['results']
     for ticker in data['results']:
         tickers.append(ticker)
+
+example_ticker = {
+    'ticker': 'BAOS', 
+    'name': 'Baosheng Media Group Holdings Limited Ordinary shares', 
+    'market': 'stocks', 
+    'locale': 'us', 
+    'primary_exchange': 'XNAS',
+    'type': 'CS', 
+    'active': True, 
+    'currency_name': 'usd', 
+    'cik': '0001811216', 
+    'last_updated_utc': '2025-09-15T06:04:58.614854517Z'
+}
 
 field_names = list(example_ticker.keys())
 output_csv = 'tickers.csv'
@@ -31,4 +46,4 @@ with open(output_csv, mode='w', newline='', encoding='utf-8') as f:
     for t in tickers:
         row = {key: t.get(key, '') for key in field_names}
         writer.writerow(row)
-print(f'Wrote {len(tickers)} rows to {output_csv}') 
+print(f'Wrote {len(tickers)} rows to {output_csv}')
