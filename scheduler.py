4fda2e26435731cfdcaff2d64dc93584b70c1f68
tomasgroque <tomas.groque@gmail.com>
2025-10-01 17:05:43 -0600

    Update requirements.txt, create run stock logic.

---
+++ scheduler.py
@@ -0,0 +1,9 @@
+import schedule
+import time
+from script import run_stock_job
+from datetime import datetime
+
+def basic_job():
+    print('Job started at: ', datetime.now())
+
+    
--- script.py
+++ script.py
@@ -7,43 +7,48 @@ load_dotenv()
 
 POLYGON_API_KEY = os.getenv('POLYGON_API_KEY')
 LIMIT = 1000
-url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}"
 
-response = requests.get(url)
+def run_stock_job():
+    url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}"
 
-tickers = []
-raw_data = response.json()
-data = raw_data['results']
+    response = requests.get(url)
 
-for ticker in data:
-    tickers.append(ticker)
-
-while 'next_url' in data:
-    response = requests.get(data['next_url'] + f'?&apiKey={POLYGON_API_KEY}')
+    tickers = []
     raw_data = response.json()
     data = raw_data['results']
-    for ticker in data['results']:
+
+    for ticker in data:
         tickers.append(ticker)
 
-example_ticker = {
-    'ticker': 'BAOS', 
-    'name': 'Baosheng Media Group Holdings Limited Ordinary shares', 
-    'market': 'stocks', 
-    'locale': 'us', 
-    'primary_exchange': 'XNAS',
-    'type': 'CS', 
-    'active': True, 
-    'currency_name': 'usd', 
-    'cik': '0001811216', 
-    'last_updated_utc': '2025-09-15T06:04:58.614854517Z'
-}
+    while 'next_url' in data:
+        response = requests.get(data['next_url'] + f'?&apiKey={POLYGON_API_KEY}')
+        raw_data = response.json()
+        data = raw_data['results']
+        for ticker in data['results']:
+            tickers.append(ticker)
 
-field_names = list(example_ticker.keys())
-output_csv = 'tickers.csv'
-with open(output_csv, mode='w', newline='', encoding='utf-8') as f:
-    writer = csv.DictWriter(f, fieldnames=field_names)
-    writer.writeheader()
-    for t in tickers:
-        row = {key: t.get(key, '') for key in field_names}
-        writer.writerow(row)
-print(f'Wrote {len(tickers)} rows to {output_csv}')
+    example_ticker = {
+        'ticker': 'BAOS', 
+        'name': 'Baosheng Media Group Holdings Limited Ordinary shares', 
+        'market': 'stocks', 
+        'locale': 'us', 
+        'primary_exchange': 'XNAS',
+        'type': 'CS', 
+        'active': True, 
+        'currency_name': 'usd', 
+        'cik': '0001811216', 
+        'last_updated_utc': '2025-09-15T06:04:58.614854517Z'
+    }
+
+    field_names = list(example_ticker.keys())
+    output_csv = 'tickers.csv'
+    with open(output_csv, mode='w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=field_names)
+        writer.writeheader()
+        for t in tickers:
+            row = {key: t.get(key, '') for key in field_names}
+            writer.writerow(row)
+    print(f'Wrote {len(tickers)} rows to {output_csv}')
+
+if __name__ == '__main__':
+    run_stock_job()
